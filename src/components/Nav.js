c1414525b2df3b89d8d169ca15b41625554cc7e3
Lina Zoo <linazooper@gmail.com>
2022-10-05 13:45:39 -0500

    creating a page for our newsletter monitization initiave

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import FutureFrontLogo from "../fftxlogo.png";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <img
+        className="logo"
+        src={FutureFrontLogo}
+        alt="future front texas logo"
+      />
+    </nav>
+  );
+};
---
+++ src/components/Tiers.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Tiers = () => {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <h1 className="header">Advertise with FFTX</h1>
+          <p className="text">
+            We design vibrant spaces, resources and opportunities for women and
+            LGBTQ+ creators in Texas.
+          </p>
+          <p className="text">
+            We'll work closely with you to create content around your brand or
+            product that our developers will love.
+          </p>
+          <div>
+            <h1 className="header">112,019</h1>
+            <p className="sub-header">Active Subscribers</p>
+            <h1 className="header">50% </h1>
+            <p className="sub-header">avg. open rate</p>
+            <h1 className="header">86%</h1>
+            <p className="sub-header">opened within the last month</p>
+          </div>
+          <div>
+            <img />
+          </div>
+          <div>
+            <h1 className="header header-color">Premier 2K</h1>
+            <p className="sub-header">1 spot per issue $1,500 - $2,500 </p>
+            <p className="text">
+              (depends on how much we want to do) If you want to tell a story
+              about your product or brand. We'll work with you to create content
+              that nonprofits, artist ran organizations, women, creators will
+              love and we'll place it in a premium spot with the newsletter (So
+              a section of the newsletter all to themselves to tell a story
+              about their product)
+            </p>
+            <h1 className="header header-color">Premier Platinum</h1>
+            <p className="sub-header">1 spot per issue $1,000 - 1,500 </p>
+            <p className="text">
+              Sponsor our ‘volunteering newsletter’ the whole thing. Your
+              company can sponsor a section of our newsletter (a section that we
+              always do)
+            </p>
+            <h1 className="header header-color">Premier Gold</h1>
+            <p className="sub-header">1 spot per issue $600 - $1000 </p>
+            <p className="text">Sponsor another section of the newsletter</p>
+            <h1 className="header header-color">Premier Silver </h1>
+            <p className="sub-header">3 spots per issue $250-$500 </p>
+            <p className="text">
+              Have a job you want to post, have information you want to get out
+              there, you want to advertise your event in our newsletter (we’ll
+              have to vouch for it)
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Tiers;
